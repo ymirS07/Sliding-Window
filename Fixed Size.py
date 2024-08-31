878b6163644456309d83d115f7d34aa6b874be61
Siqi Yang <144865198+ymirS07@users.noreply.github.com>
2024-08-31 17:42:45 +0800

    Create Fixed Size.py

---
+++ Fixed Size.py
@@ -0,0 +1,12 @@
+def sliding_window(nums, k):
+  window = []
+  res = []
+  
+  left = 0
+  for right in range(len(nums)):
+    window.append(nums[right])
+    if right - left + 1 == k:
+      res.append(some_operation(window))
+      window.pop()
+      left += 1
+  return res
